1a749edfb3f78df99f9df1d0ca0d784d78c8a546
Fabian Fuchs <fuchs.fabian@posteo.de>
2024-11-11 00:32:40 +0100

    Improve setup.bash

--- setup.bash
+++ setup.bash
@@ -1,27 +1,68 @@
 #!/usr/bin/env bash
 
-APP_NAME="domposy"
-APP_BRANCH_NAME="v2"
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░                VARIABLES                 ░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
 
-LOGGER_NAME="simbashlog"
-LOGGER_BRANCH_NAME="main"
+declare -r CONST_APP_NAME="domposy"
+declare -r CONST_APP_BRANCH_NAME="v2"
+
+declare -r CONST_LOGGER_NAME="simbashlog"
+declare -r CONST_LOGGER_BRANCH_NAME="v1.1.3"
+
+declare -r CONST_GLOBAL_DOWNLOAD_PATH="/opt"
+declare -r CONST_GLOBAL_BIN_PATH="/usr/bin"
+
+declare -r CONST_USER_DOWNLOAD_PATH="$HOME/.local/share"
+declare -r CONST_USER_BIN_PATH="$HOME/.local/bin"
+
+declare -r CONST_DEPENDENCIES=(
+    "git"
+    "docker"
+    "tar"
+    "gzip"
+)
 
 # NOTE: The repos must contain a directory called 'src' with a bash script that has the same name as the repo and ends with '.bash'!
-REPO_URLS=(
-    "https://github.com/fuchs-fabian/${LOGGER_NAME}.git"
-    "https://github.com/fuchs-fabian/${APP_NAME}.git"
+declare -r CONST_REPO_URLS=(
+    "https://github.com/fuchs-fabian/${CONST_LOGGER_NAME}.git"
+    "https://github.com/fuchs-fabian/${CONST_APP_NAME}.git"
 )
 
-DEPENDENCIES=(
-    "git"
-    "rsync"
-)
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░                  LOGIC                   ░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
 
-OPT_DIR="/opt"
+function get_app_name_from_repo_url {
+    basename "$1" .git
+}
+
+function is_root {
+    [[ $(id -u) -eq 0 ]]
+}
+
+# ╔═════════════════════╦══════════════════════╗
+# ║                                            ║
+# ║                INSTALLATION                ║
+# ║                                            ║
+# ╚═════════════════════╩══════════════════════╝
 
 function install {
+    local download_path=""
+    local bin_path=""
+
+    echo "Current user: '$(whoami)'"
+
     function check_dependencies {
-        for dependency in "${DEPENDENCIES[@]}"; do
+        for dependency in "${CONST_DEPENDENCIES[@]}"; do
             echo "Checking if '$dependency' is available..."
             command -v "$dependency" >/dev/null 2>&1 ||
                 {
@@ -32,84 +73,141 @@ function install {
         done
     }
 
-    function get_app_name_from_repo_url {
-        basename "$1" .git
+    function create_directory {
+        local directory="$1"
+
+        if [ ! -d "$directory" ]; then
+            echo "Creating directory '$directory'..."
+            mkdir -p "$directory"
+        fi
     }
 
-    read -r -p "Do you want to install the $APP_NAME globally ('g') or just for the current user ('u')? (g/u): " bin_dir_choice
-    if [[ "$bin_dir_choice" =~ ^[Gg]$ ]]; then
-        # Symlink in /usr/bin for all users
-        bin_path="/usr/bin"
-    else
-        # Symlink in ~/bin for the current user
-        bin_path="$HOME/bin"
-    fi
-
-    for repo_url in "${REPO_URLS[@]}"; do
-        app_name=$(get_app_name_from_repo_url "$repo_url")
-        app_dir="$OPT_DIR/$app_name"
-
-        if [ -d "$app_dir" ]; then
-            echo "$app_name is already downloaded."
+    function set_paths {
+        if is_root; then
+            download_path="$CONST_GLOBAL_DOWNLOAD_PATH"
+            bin_path="$CONST_GLOBAL_BIN_PATH"
         else
-            read -r -p "$app_name is not downloaded. Do you want to download it? (y/n): " choice
-            if [[ "$choice" =~ ^[Yy]$ ]]; then
-                echo "Cloning $app_name..."
+            echo "You are not root."
+            echo "The installation will be only available for the current user."
+            echo "To install it globally, please run this script as root (sudo)."
 
-                if [[ "$app_name" == "$APP_NAME" ]]; then
-                    git clone --branch "$APP_BRANCH_NAME" "$repo_url" "$app_dir"
-                elif [[ "$app_name" == "$LOGGER_NAME" ]]; then
-                    git clone --branch "$LOGGER_BRANCH_NAME" "$repo_url" "$app_dir"
-                else
-                    git clone "$repo_url" "$app_dir"
-                fi
+            read -r -p "Do you want to continue? [Y/n] " choice
+            if [[ "$choice" =~ ^[Yy]$ ]]; then
+                download_path="$CONST_USER_DOWNLOAD_PATH"
+                bin_path="$CONST_USER_BIN_PATH"
+
+                create_directory "$CONST_USER_DOWNLOAD_PATH"
+                create_directory "$CONST_USER_BIN_PATH"
+            else
+                echo "Installation aborted."
+                exit 1
             fi
         fi
 
-        if [ -f "$app_dir/src/${app_name}.bash" ]; then
-            chmod +x "$app_dir/src/${app_name}.bash"
+        echo "Donwload path: '$download_path'"
+        echo "Bin path: '$bin_path'"
+    }
 
-            echo "Creating symlink for $app_name..."
-            ln -sf "$app_dir/src/${app_name}.bash" "$bin_path/$app_name"
-            echo "$app_name has been installed and is now executable."
+    check_dependencies
+    set_paths
+
+    for repo_url in "${CONST_REPO_URLS[@]}"; do
+        local app_name
+        app_name=$(get_app_name_from_repo_url "$repo_url")
+
+        local app_path="$download_path/$app_name"
+
+        if [ -d "$app_path" ]; then
+            echo "'$app_name' is already downloaded."
+        else
+            echo "Downloading '$app_name'..."
+
+            if [[ "$app_name" == "$CONST_LOGGER_NAME" ]]; then
+                echo "Cloning '$CONST_LOGGER_NAME' from branch '$CONST_LOGGER_BRANCH_NAME'..."
+                git clone --branch "$CONST_LOGGER_BRANCH_NAME" "$repo_url" "$app_path"
+
+            elif [[ "$app_name" == "$CONST_APP_NAME" ]]; then
+                echo "Cloning '$CONST_APP_NAME' from branch '$CONST_APP_BRANCH_NAME'..."
+                git clone --branch "$CONST_APP_BRANCH_NAME" "$repo_url" "$app_path"
+
+            else
+                echo "Cloning $app_name..."
+                git clone "$repo_url" "$app_path"
+            fi
+        fi
+
+        local original_app_path="$app_path/src/${app_name}.bash"
+
+        if [ -f "$original_app_path" ]; then
+            echo "Making '$app_name' executable..."
+            chmod +x "$original_app_path"
+
+            echo "Creating symlink for '$app_name'..."
+            ln -sf "$original_app_path" "$bin_path/$app_name"
+            echo "'$app_name' has been installed and is now executable."
 
             if [ -n "$(command -v "$app_name")" ]; then
                 "$app_name" -v
-                echo "$app_name is working."
+                echo "'$app_name' is working."
             else
-                echo "$app_name is not working."
+                echo "'$app_name' is not working."
             fi
         else
-            echo "The file ${app_name}.bash was not found in the directory $app_dir/src."
+            echo "The file '${app_name}.bash' was not found in '$app_path/src'."
         fi
     done
 }
+
+# ╔═════════════════════╦══════════════════════╗
+# ║                                            ║
+# ║               UNINSTALLATION               ║
+# ║                                            ║
+# ╚═════════════════════╩══════════════════════╝
 
 function uninstall {
-    local bin_paths=(
-        "/usr/bin"
-        "$HOME/bin"
-    )
+    local download_path=""
+    local bin_path=""
 
-    for repo_url in "${REPO_URLS[@]}"; do
-        app_name=$(basename "$repo_url" .git)
-        app_dir="$OPT_DIR/$app_name"
-
-        for bin_path in "${bin_paths[@]}"; do
-            if [ -L "$bin_path/$app_name" ]; then
-                echo "Removing symlink for $app_name in $bin_path..."
-                rm "$bin_path/$app_name"
-            fi
-        done
-
-        if [ -d "$app_dir" ]; then
-            echo "Removing directory $app_dir..."
-            rm -rf "$app_dir"
+    function set_paths {
+        if is_root; then
+            download_path="$CONST_GLOBAL_DOWNLOAD_PATH"
+            bin_path="$CONST_GLOBAL_BIN_PATH"
         else
-            echo "$app_name is not installed."
+            download_path="$CONST_USER_DOWNLOAD_PATH"
+            bin_path="$CONST_USER_BIN_PATH"
+        fi
+
+        echo "Donwload path: '$download_path'"
+        echo "Bin path: '$bin_path'"
+    }
+
+    set_paths
+
+    for repo_url in "${CONST_REPO_URLS[@]}"; do
+        app_name=$(get_app_name_from_repo_url "$repo_url")
+        app_path="$download_path/$app_name"
+
+        if [ -L "$bin_path/$app_name" ]; then
+            echo "Removing symlink for '$app_name' in '$bin_path'..."
+            rm "$bin_path/$app_name"
+        fi
+
+        if [ -d "$app_path" ]; then
+            echo "Removing directory '$app_path'..."
+            rm -rf "$app_path"
+        else
+            echo "'$app_name' is not installed."
         fi
     done
 }
+
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░                  MAIN                    ░░
+# ░░                                          ░░
+# ░░                                          ░░
+# ░░░░░░░░░░░░░░░░░░░░░▓▓▓░░░░░░░░░░░░░░░░░░░░░░
 
 case "$1" in
 install)
